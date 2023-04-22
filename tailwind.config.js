83d46f73386e882bc6d02794fa617509c53b1a17
Luiz Henrique <lhmorenoti@hotmail.com>
2023-04-22 15:02:01 -0300

    style: change container width and scrollbar style

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,10 @@ module.exports = {
         }
       },
 
+      screens: {
+        '2xl': '1440px'
+      },
+
       backgroundImage: {
         'gradient-vertical': 'linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)',
         'gradient-horizontal': 'linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)'
